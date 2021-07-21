1136b7ceb30911f560f4e2eaa86452a94ac6ae0d
ivantabera <ivantabera19@gmail.com>
2021-07-21 18:21:06 -0500

    primer commit donde interactuamos con e.preventdefault para evitar que recargue la pagina

---
+++ src/GifExpertApp.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory'
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One punch', 'Samurai X', 'Dragon ball'])
+
+    //dos maneras de agregar un elemento al arreglo
+    /* const handleAdd = () => {
+        // setCategories([...categories, 'Bob Sponja']);
+        setCategories( cate => [...categories, 'Bob sponja']);
+    } */
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory/>
+            
+            <hr/>
+
+            <ol>
+                {
+                    categories.map( (category) => {
+                        return <li key={category}> {category} </li>
+                    })
+                }
+            </ol>
+        </>
+    )
+}
