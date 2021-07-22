de2c32a5c41d03472e9bc7a034943ab4af3c5d0d
ivantabera <ivantabera19@gmail.com>
2021-07-22 12:11:58 -0500

    Se realizo la implementacion de useEffect para evitar que se ejecute la API cada vez que damos click en algun elemento o exista algun cambio en el estado

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const GifGrid = ({ category }) => {
 
+    const [count, setCount] = useState(0);
+
+    // Declaramos la funcion getGif dentro del useEffect() para que solo se ejecute 1 ves cuando cargue la pagina
+    // El Hook de efecto te permite llevar a cabo efectos secundarios en componentes funcionales:
+    useEffect( () => {
+        getGif();
+    }, []);
+
     // haremos una funcion asincrona para recibir los datos de la API
-    const gitGif = async() => {
+    const getGif = async() => {
 
         // Guardamos nuestra url del api en una constante
         const url = 'https://api.giphy.com/v1/gifs/search?q=Rick and Morty&limit=10&api_key=fb36rhWkdzSVD5NPLfFas2Wryuq82AM2';
@@ -21,18 +29,16 @@ export const GifGrid = ({ category }) => {
                 url: img.images.downsized_medium.url
             }
         })
-
         console.log(gifs)
-
-
     }
 
-    gitGif();
-
+    // gitGif();
 
     return (
         <div>
             <h3> { category } </h3>
+            <h2>{count}</h2>
+            <button onClick={ () => setCount( count + 1) }>+</button>
         </div>
     )
 }
