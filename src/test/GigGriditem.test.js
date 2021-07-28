d39ab2c73e7e1637684e82d823463dd8e14abdb7
ivantabera <ivantabera19@gmail.com>
2021-07-28 12:53:38 -0500

    Se generaron pruebas en nuestro compontente GigGridItem

--- src/test/GigGriditem.test.js
+++ src/test/GigGriditem.test.js
@@ -7,19 +7,30 @@ describe('Pruebas en el grid item', () => {
 
     const title = 'Hola mundo';
     const url =  'https://holamundo.com';
+    const wrapper = shallow( <GigGriditem title = { title } url = {url} /> );
 
     test('Mostrar el componente correctamente', () => {
-        
-
-        const wrapper = shallow( 
-            <GigGriditem 
-                title = { title } 
-                url = {url} 
-            /> 
-        );
-
         expect( wrapper ).toMatchSnapshot();
-
     })
+
+    test('Debe tener un parrafo con el Title', () => {
+        const p = wrapper.find('p');
+        expect( p.text().trim()).toBe( title );
+    })
+
+    test('Comprobar que tenemos un img con url y alt valido similar al de los props', () => {
+        const img = wrapper.find('img');
+        expect(img.prop('src')).toBe(url);
+        expect(img.prop('alt')).toBe(title);
+    })
+
+    test('Comprobar que el div contenga el className "animate__fadeIn"', () => {
+        const div = wrapper.find('div');
+        const className = div.prop('className');
+        const animacion = className.includes('animate__fadeIn');
+        expect( animacion ).toBe(true);
+    })
+    
+    
     
 });
