482ce5d91d1e1152837e134d9eb235c2b3ae85eb
ivantabera <ivantabera19@gmail.com>
2021-08-03 20:26:55 -0500

    CustomHook waitForNextUpdate

--- src/test/hooks/useFetchGifsf.test.js
+++ src/test/hooks/useFetchGifsf.test.js
@@ -3,17 +3,26 @@ import { renderHook } from '@testing-library/react-hooks'
 
 describe('Pruebas en el hook useFetchGifs', () => {
 
-    test('Debe de retornar el estado inicial ', () => {
-        
+    test('Debe de retornar el estado inicial ', async() => {
         const category = 'dragon ball';
-        
-        const {result} = renderHook( () => useFetchGifs(category))
+        const {result, waitForNextUpdate} = renderHook( () => useFetchGifs(category))
         const { data, loading } =  result.current ;
         
+        await waitForNextUpdate();
+
         expect( data ).toEqual([]);
         expect( loading ).toBe(true);
+    })
+    
+    test('Debe de retornar un arreglo de imagenes y el loading en false', async() => {
+        const category = 'dragon ball';
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs(category));
         
-
+        await waitForNextUpdate();
+        const { data, loading } =  result.current ;
+        
+        expect( data.length ).toEqual(10);
+        expect( loading ).toBe(false);
     })
     
 })
